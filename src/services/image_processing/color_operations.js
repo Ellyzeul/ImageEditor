a95a1ad1d2db8d47a4981e066e7a576ad4756ad7
Ellyzeul <gabrielaug.240@gmail.com>
2021-07-19 23:38:31 -0300

    feat: adding RGB conversion to HSL (not implemented on frontend though)

--- src/services/image_processing/color_operations.js
+++ src/services/image_processing/color_operations.js
@@ -36,3 +36,39 @@ const removeBlue = imgData => {
     
     return imgData
 }
+
+const RGBtoHSL = (r, g, b) => {
+    r /= 255
+    g /= 255
+    b /= 255
+
+    const minRGB = Math.min(r, g, b)
+    const maxRGB = Math.max(r, g, b)
+    const chroma = maxRGB - minRGB
+
+    let h = 0
+    let s = 0
+    let l = 0
+
+    if (chroma == 0)      h = 0
+    else if (maxRGB == r) h = ((g - b) / chroma) % 6
+    else if (maxRGB == g) h = (b - r) / chroma + 2
+    else                  h = (r - g) / chroma + 4
+
+    h = Math.round(h * 40)
+
+    if (h < 0) h += 240
+
+    l = (maxRGB + minRGB) / 2
+
+    s = chroma == 0 ? 0 : chroma / (1 - Math.abs(2 * l - 1))
+
+    s = Math.round(s * 240)
+    l = Math.round(l * 240)
+
+    return {
+        "hue": h,
+        "saturation": s,
+        "lightness": l
+    }
+}
